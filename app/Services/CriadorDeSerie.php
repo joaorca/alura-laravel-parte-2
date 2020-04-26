26cfa1369d22240991a7ee2f4965e9a7e897546f
João R C Almeida <joaorca@unisuam.edu.br>
2020-04-25 21:26:10 -0300

    Resposta Desafio

--- app/Services/CriadorDeSerie.php
+++ app/Services/CriadorDeSerie.php
@@ -3,6 +3,8 @@
 namespace App\Services;
 
 use App\Serie;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class CriadorDeSerie
 {
@@ -12,15 +14,26 @@ class CriadorDeSerie
         int $epPorTemporada
     ): Serie
     {
+        DB::beginTransaction();
         $serie = Serie::create(['nome' => $nome]);
-
-        for ($i = 1; $i <= $qtdTemporadas; $i++) {
-            $temporada = $serie->temporadas()->create(['numero' => $i]);
-            for ($j = 1; $j <= $epPorTemporada; $j++) {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
-        }
+        $this->criarTemporadas($serie, $qtdTemporadas, $epPorTemporada);
+        DB::commit();
 
         return $serie;
+    }
+
+    public function criarTemporadas(Serie $serie, int $qtdTemporadas, int $epPorTemporada)
+    {
+        for ($i = 1; $i <= $qtdTemporadas; $i++) {
+            $temporada = $serie->temporadas()->create(['numero' => $i]);
+            $this->criarEpisodios($temporada, $epPorTemporada);
+        }
+    }
+
+    public function criarEpisodios(Model $temporada, int $epPorTemporada)
+    {
+        for ($j = 1; $j <= $epPorTemporada; $j++) {
+            $temporada->episodios()->create(['numero' => $j]);
+        }
     }
 }
