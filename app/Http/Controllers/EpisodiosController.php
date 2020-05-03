e0ff22a65a6de966896c4fae6fe1dde8b9d8aa93
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-02 23:28:10 -0300

    Assistindo os episódios

--- app/Http/Controllers/EpisodiosController.php
+++ app/Http/Controllers/EpisodiosController.php
@@ -2,14 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Episodio;
 use App\Temporada;
+use Illuminate\Http\Request;
 
 class EpisodiosController extends Controller
 {
     public function index(Temporada $temporada)
     {
-        $episodios = $temporada->episodios;
+        return view('episodios.index', [
+            'episodios' => $temporada->episodios,
+            'temporadaId' => $temporada->id
+        ]);
+    }
 
-        return view('episodios.index', compact('episodios'));
+    public function assistir(Temporada $temporada, Request $request)
+    {
+        $episodiosAssistidos = $request->episodios;
+        $temporada->episodios->each(function (Episodio $episodio)
+        use ($episodiosAssistidos) {
+            $episodio->assistido = in_array(
+                $episodio->id,
+                $episodiosAssistidos
+            );
+        });
+        $temporada->push();
     }
 }
--- resources/views/episodios/index.blade.php
+++ resources/views/episodios/index.blade.php
@@ -5,12 +5,15 @@
 @endsection
 
 @section('conteudo')
-    <form action="">
+    <form action="/temporadas/{{$temporadaId}}/episodios/assistir" method="post">
+        @csrf
         <ul class="list-group">
             @foreach ($episodios as $episodio)
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     Episódio {{$episodio->numero}}
-                    <input type="checkbox">
+                    <input type="checkbox"
+                           name="episodios[]"
+                           value="{{$episodio->id}}">
                 </li>
             @endforeach
         </ul>
